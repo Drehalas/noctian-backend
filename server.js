d7c64a5a271727db43508ca458fe8d7581df923d
Yasin Onur Yücel <yoy1992@gmail.com>
2024-09-12 15:15:33 +0200

    feat: add endpoints warRoutes

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const ladderRoutes = require('./routes/ladderRoutes');
 const skillBuffRoutes = require('./routes/skillBuffRoutes');
 const artifactRoutes = require('./routes/artifactRoutes');
 const heroRoutes = require('./routes/heroRoutes');
-const armoryRoutes = require('./routes/armoryRoutes'); // Import armory routes
+const armoryRoutes = require('./routes/armoryRoutes');
+const warRoutes =require('./routes/warRoutes');
 
 // Initialize the Express application
 const app = express();
@@ -30,7 +31,8 @@ app.use('/api/artifact', artifactRoutes);
 app.use('/api/ladder', ladderRoutes);
 app.use('/api/skillbuff', skillBuffRoutes);
 app.use('/api/hero', heroRoutes);
-app.use('/api/armory', armoryRoutes); // Use armory routes
+app.use('/api/armory', armoryRoutes);
+app.use('/api/nextwar', warRoutes); // Use armory routes
 
 app.get('/', (req, res) => {
     res.send('Hello, Noctian Universe!');
