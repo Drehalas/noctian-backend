124f57f4ad15913dcc75273932d07e2be1083ba3
ersinhamzakose <ehkproje@gmail.com>
2024-09-13 19:57:24 +0300

    cors hatası için düzenleme yapıldı

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config(); // Load environment variables
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db'); // Import the MongoDB connection function
 const userRoutes = require('./routes/userRoutes');
@@ -11,12 +12,20 @@ const skillBuffRoutes = require('./routes/skillBuffRoutes');
 const artifactRoutes = require('./routes/artifactRoutes');
 const heroRoutes = require('./routes/heroRoutes');
 const armoryRoutes = require('./routes/armoryRoutes');
-const warRoutes =require('./routes/warRoutes');
+const warRoutes = require('./routes/warRoutes');
 const soldierRoutes = require('./routes/soldierRoutes');
 const userAuthRoutes = require('./routes/userAuthRoutes');
 
 // Initialize the Express application
 const app = express();
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // Middleware
 app.use(bodyParser.json());
